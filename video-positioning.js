47e52de079db6f4b9d3bc9a359a3ce5f29f4a683
Rob McCormack <mccormac@confederationc.on.ca>
2018-02-27 15:43:31 -0500

    added button at top

--- video-positioning.js
+++ video-positioning.js
@@ -1,5 +1,12 @@
 const {Button, Video, ui} = require('tabris');
 
+new Button({
+  left: 0, top:0,
+  text: 'Get'
+}).on('select', () => {
+
+}).appendTo(ui.contentView);
+
 let button = new Button({
   id: 'button',
   centerX: 0, bottom: 16,
@@ -9,7 +16,7 @@ let button = new Button({
 
 let video = new Video({
   left: 0, top: 0, right: 0, bottom: '#button 16',
-  url: 'http://peach.themazzone.com/durian/movies/sintel-1280-stereo.mp4',
+  url: 'https://player.vimeo.com/external/135788180.sd.mp4?s=0a366925ff572514f5bca12f4bc38df0e7ec1a14&profile_id=112',
   controlsVisible: false
 }).on('stateChanged', event => button.text = event.value !== 'pause' ? '❚❚' : '▶')
   .appendTo(ui.contentView);
