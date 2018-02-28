0d0dd3a946dfddfd3250d694366079912e6c022e
Rob (mysignaturr@gmail.com) <mysignaturr@gmail.com>
2018-02-28 11:47:05 -0500

    Tidy up code, use global for entering

--- JUNK-dice-game-dice-sidebyside.js
+++ JUNK-dice-game-dice-sidebyside.js
@@ -1,18 +1,18 @@
-const {ui, ImageView} = require('tabris');
-
+const { ui, ImageView} = require('tabris');
 // example of images side by side - Matt.
 
 const IMAGE_PATH = 'https://mrmccormack.github.io/imd-learning-tabris/images/';
+const DICE_OFFSET = 30;
 
 let diceImage1 = new ImageView({
-  centerY:0,
-  left: '33%',
+  centerY: 0,
+  centerX: -DICE_OFFSET,
   image: IMAGE_PATH + '6.png'
 }).appendTo(ui.contentView);
 
 let diceImage2 = new ImageView({
-  top: 'prev() -50',
-  right: diceImage1.left,
+  centerY: 0,
+  centerX: DICE_OFFSET,
   image: IMAGE_PATH + '1.png'
 
 }).appendTo(ui.contentView);
