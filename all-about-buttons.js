355c17be215b74ed875cac9f71ee10b020926daa
Rob (mysignaturr@gmail.com) <mysignaturr@gmail.com>
2018-02-28 09:33:55 -0500

    added two reset button, with event outside and inside

--- all-about-buttons.js
+++ all-about-buttons.js
@@ -22,15 +22,32 @@ let togglebutton2 = new ToggleButton({
   .appendTo(ui.contentView);
 
 
-  let resetbutton = new Button({
+// button with event outside
+let resetbutton = new Button({
     centerX: 0,
     top: 'prev() 10',
     text: 'Reset',
+    image: 'https://github.com/mrmccormack/imd-learning-tabris/blob/master/images/card.png?raw=true'
+  })
+.appendTo(ui.contentView);
+
+// event outsite create new
+resetbutton.on('select', () => {
+console.log ('you pressed reset');
+console.log (IMAGE_PATH);
+  })
+
+
+// button with event inside
+  let resetbutton1 = new Button({
+    centerX: 0,
+    top: 'prev() 10',
+    text: 'Reset1',
     // for some reason, constant and image no work???
     image: 'https://github.com/mrmccormack/imd-learning-tabris/blob/master/images/card.png?raw=true'
   })
   .on('select', () => {
-console.log ('you pressed reset');
+console.log ('you pressed reset1');
 console.log (IMAGE_PATH);
 
 
