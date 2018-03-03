519f05aa16200bf57585c0bbe0362145470f4eb1
Rob (mysignaturr@gmail.com) <mysignaturr@gmail.com>
2018-03-03 10:02:53 -0500

    beautify code

--- classes-button.js
+++ classes-button.js
@@ -7,34 +7,45 @@ const {Button, ui} = require('tabris');
 
 let btnOK = new Button({
   class: 'main',
-  centerX:0, top: 'prev() 10', width:100,
+  centerX: 0,
+  top: 'prev() 10',
+  width: 100,
   text: 'OK'
 }).appendTo(ui.contentView);
 
 let btnCancel = new Button({
   class: 'main',
-  centerX:0, top: 'prev() 10', width:100,
+  centerX: 0,
+  top: 'prev() 10',
+  width: 100,
   text: 'Cancel'
 }).appendTo(ui.contentView);
 
 let btnReset = new Button({
   id: 'reset',
-  centerX:0, top: 'prev() 10', width:100,
+  centerX: 0,
+  top: 'prev() 10',
+  width: 100,
   text: 'Reset'
 }).appendTo(ui.contentView);
 
 // event outsite create new declaration
 btnReset.on('select', () => {
 
-//  using classes
-ui.find('.main').set('textColor', 'red');
-ui.apply({'*': {background: '#ccc', width: 130}}); // apply using wildcards and multiple properties
+  //  using classes
+  ui.find('.main').set('textColor', 'red');
+  ui.apply({
+    '*': {
+      background: '#ccc',
+      width: 130
+    } // end .apply
+  }); // apply using wildcards and multiple properties
 
-// using id
-ui.find('#reset').set('textColor', 'white');
-ui.find('#reset').set('background', '#f00');
-ui.find('#reset').set('textColor', 'white');
+  // using id
+  ui.find('#reset').set('textColor', 'white');
+  ui.find('#reset').set('background', '#f00');
+  ui.find('#reset').set('textColor', 'white');
 
-console.log ('you pressed btnReset');
+  console.log('you pressed btnReset');
 
-})  // end event btnReset on('select')
+}) // end event btnReset on('select')
