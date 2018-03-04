c062e48da40c279f9792fcd63a60e4da761fef90
Rob (mysignaturr@gmail.com) <mysignaturr@gmail.com>
2018-03-03 19:52:48 -0500

    added comment header

--- button-view-on-github.js
+++ button-view-on-github.js
@@ -1,10 +1,28 @@
+/*
+  Using classes and id's
+
+  @version:2.4
+  Snippet url:
+  -  http://
+  References:
+  - https://tabrisjs.com/documentation/latest/selector.html
+
+*/
+
 const {ImageView, Button,app, ui} = require('tabris');
 
-const GITHUB_URL = 'https://github.com/mrmccormack/tabris_playground/blob/master/snippets/pg-imageview-scalemode.js';
+
+const GITHUB_URL = 'https://github.com/mrmccormack/imd-learning-tabris/blob/master/button-view-on-github.js';
+
+let imgTabrisIcon = new ImageView({
+    centerX: 0, top: 'prev() 10',
+    image: 'https://raw.githubusercontent.com/mrmccormack/imd-learning-tabris/master/images/tabris-icon.png',
+    scaleMode: 'none'
+}).appendTo(ui.contentView);
 
 
 let imgGithubLink = new ImageView({
-    centerX: 0, top: 'prev() 10',
+    centerX: 0, bottom: 0,
     image: 'https://cdn3.iconfinder.com/data/icons/free-social-icons/67/github_circle_black-48.png',
     scaleMode: 'none'
 }).on({
