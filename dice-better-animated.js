629d9c4a4d8d887566194a2ae3a8728c24def05f
Rob McCormack <mccormac@confederationc.on.ca>
2018-03-14 19:06:37 -0400

    dice working

---
+++ dice-better-animated.js
@@ -0,0 +1,71 @@
+const { Button, ui, ImageView} = require('tabris');
+const IMAGE_PATH = 'https://mrmccormack.github.io/imd-learning-tabris/images/';
+const DICE_OFFSET = 30;
+
+diceImage1 = new ImageView({
+
+  left: 150, width: 50, height: 50,
+
+  image: IMAGE_PATH + '1.jpg'
+}).appendTo(ui.contentView);
+
+let diceImage2 = new ImageView({
+    left: 50,  width: 50, height: 50,
+  image: IMAGE_PATH + '6.jpg'
+
+}).appendTo(ui.contentView);
+
+
+// button with event inside
+  let btnAnimate = new Button({
+     left: 50,
+    top: 'prev() 10',
+    text: 'Animate',
+
+  })
+  .on('select', () => {
+//console.log ('you pressed btnAnimate');
+
+let rand = 1 + Math.floor(Math.random() * 6);
+diceImage1.image = IMAGE_PATH + rand + '.jpg';
+
+let rand1 = 1 + Math.floor(Math.random() * 6);
+diceImage2.image = IMAGE_PATH + rand1 + '.jpg';
+
+totalDice = rand1 + rand;
+btnAnimate.text = 'You rolled: ' + totalDice;
+
+diceImage2.animate({
+  //opacity: 0,
+
+  transform: {
+
+    rotation: 2 * Math.PI,
+    //scaleX: 2
+  }
+
+
+}, {
+  duration: 300,
+  easing: 'ease-out',
+          repeat: 1,
+    reverse: true,
+}).then(() => label.dispose());
+
+
+    diceImage1.animate({
+  //opacity: 0,
+
+  transform: {
+    rotation: 4 * Math.PI,
+    //scaleX: 2
+  }
+}, {
+  duration: 500,
+  easing: 'ease-out',
+          repeat: 1,
+    reverse: true,
+}).then(() => label.dispose());
+
+
+  }).appendTo(ui.contentView);
