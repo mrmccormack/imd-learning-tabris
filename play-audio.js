59d29fd23427297dda913994744dad087780b822
Rob McCormack <mccormac@confederationc.on.ca>
2018-03-30 13:57:11 -0400

    foratted code
    I posted this as a question to Stackoverflow - sonce autio only plays once.

--- play-audio.js
+++ play-audio.js
@@ -1,14 +1,13 @@
 // simple example to play audio file
 
-
 const {Button, ui} = require('tabris');
-
 var my_media = new Media('http://static1.grsites.com/archive/sounds/cartoon/cartoon001.mp3');
-//my_media.play();
 
 let btnPlay = new Button({
-  centerX: 0, centerY: 0,
-    text: 'Play sound',
+  centerX: 0,  centerY: 0,
+  text: 'Play sound',
+  background: 'blue',
+  textColor: 'white',
   font: '24px'
 }).on("select", function() {
   my_media.play();
