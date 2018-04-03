4a85948c8717ac71eb39ada7e40e9ecde9abc9e0
Rob McCormack <imedia@imd-im-49a.camp.confederationc.on.ca>
2018-04-03 14:29:44 -0400

    submitted issue to Tabris on this one.

--- play-video.js
+++ play-video.js
@@ -1,15 +1,8 @@
-const {Button, Video, ui} = require('tabris');
-
-let button = new Button({
-  id: 'button',
-  centerX: 0, bottom: 16,
-  text: '❚❚'
-}).on('select', () => video.state === 'play' ? video.pause() : video.play())
-  .appendTo(ui.contentView);
+const {Video, ui} = require('tabris');
 
 let video = new Video({
-  left: 0, top: 0, right: 0, bottom: '#button 16',
+  left: 0, top: 0, right: 0,
   controlsVisible: true,
+  autoPlay: true,
   url: 'http://peach.themazzone.com/durian/movies/sintel-1280-stereo.mp4',
-}).on('stateChanged', event => button.text = event.value !== 'pause' ? '❚❚' : '▶')
-  .appendTo(ui.contentView);
+}).appendTo(ui.contentView);
