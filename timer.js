8325ead707fd218b3fd9a3c356d2413f064e1d2f
Rob McCormack <imedia@IMD-IM-61a-2.hitronhub.home>
2018-04-05 21:19:41 -0400

    minor fixes

--- timer.js
+++ timer.js
@@ -1,26 +1,28 @@
 const { Button, ActivityIndicator, ui } = require('tabris')
 
 const interval = 2000
+const btnText = 'Press me'
 
 let activityIndicator = new ActivityIndicator({
   centerX: 0,
   top: 200,
+  tintColor: '#f00',
   visible: false
 }).appendTo(ui.contentView)
 
 let btnWait = new Button({
   centerX: 0,
   centerY: 0,
-  text: 'Press me!'
+  text: btnText
 }).appendTo(ui.contentView)
 
 btnWait.on('select', ({ target }) => {
-  target.text = 'Please wait...'
+  target.text = 'Please wait ' + interval/1000 + ' seconds...'
   activityIndicator.visible = true
   setTimeout(sayThanks, interval, target)
 })
 
 function sayThanks (widget) {
-  widget.text = 'Thank you!'
   activityIndicator.visible = false
+  widget.text = btnText
 }
