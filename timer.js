5071e7ecdadb007822fbd93ad945e6e6262ddb95
Rob McCormack <imedia@IMD-IM-61a-2.hitronhub.home>
2018-04-05 21:04:45 -0400

    added activity indicator

--- timer.js
+++ timer.js
@@ -1,6 +1,12 @@
-const { Button, ui } = require('tabris')
+const { Button, ActivityIndicator, ui } = require('tabris')
 
 const interval = 2000
+
+let activityIndicator = new ActivityIndicator({
+  centerX: 0,
+  top: 200,
+  visible: false
+}).appendTo(ui.contentView)
 
 let btnWait = new Button({
   centerX: 0,
@@ -10,9 +16,11 @@ let btnWait = new Button({
 
 btnWait.on('select', ({ target }) => {
   target.text = 'Please wait...'
+  activityIndicator.visible = true
   setTimeout(sayThanks, interval, target)
 })
 
 function sayThanks (widget) {
   widget.text = 'Thank you!'
+  activityIndicator.visible = false
 }
