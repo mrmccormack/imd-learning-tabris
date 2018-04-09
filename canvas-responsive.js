a67c6a4ad6afd04da999e1fa32828c0080a70464
Rob McCormack <imedia@IMD-IM-61a-2.hitronhub.home>
2018-04-09 11:44:35 -0400

    improving canvas and positioning

--- canvas-responsive.js
+++ canvas-responsive.js
@@ -1,27 +1,33 @@
 const { Canvas, ui } = require('tabris')
 // Ref: https://developer.mozilla.org/en-US/docs/Web/API/Canvas_API/Tutorial/Drawing_shapes
-new Canvas({ left: 0, top: 0, right: 0, bottom: 0 })
+ui.contentView.background = '#f00'
+
+new Canvas({ centerX: 0, centerY: 0 })
   .on('resize', ({ target: canvas, width, height }) => {
     let ctx = canvas.getContext('2d', width, height)
     let posX = width / 2
     let posY = height / 2
-    ctx.moveTo(posX, posY)
-    ctx.beginPath()
-    ctx.arc(posX, posY, 93, 0, 2 * Math.PI, true)
-    ctx.fillStyle = '#B3E5FC'
-    ctx.fill()
 
-    // draw the stroke
-    ctx.lineWidth = 20
-    ctx.strokeStyle = '#0288D1'
-    ctx.stroke()
+    let topMargin = height / 3
+    let topOffSet = 15
+    let rectWidth = 60
+    let rectHeight = rectWidth
 
-    ctx.moveTo(0, 0)
-    ctx.beginPath()
+    ctx.fillStyle = '#db4437'
+    ctx.fillRect(posX - 40, topMargin, rectWidth, rectHeight)
+    ctx.fillStyle = '#3f51b5'
+    ctx.fillRect(posX, topMargin + topOffSet, rectWidth, rectHeight)
+    ctx.fillStyle = '#8dbd00'
+    ctx.fillRect(
+      posX + 25,
+      topMargin + topOffSet * 2,
+      rectWidth,
+      rectHeight
+    )
     ctx.font = '24px'
     ctx.fillStyle = '#212121'
     ctx.textAlign = 'center'
 
-    ctx.fillText('Logo', posX, posY)
+    ctx.fillText('Logo', posX, posY + 80)
   })
   .appendTo(ui.contentView)
