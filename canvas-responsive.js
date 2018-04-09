4942ee87bf5ac1431693cf7ac20d787219b7fa2f
Rob McCormack <imedia@IMD-IM-61a-2.hitronhub.home>
2018-04-09 12:48:20 -0400

    went with Unity Colors

--- canvas-responsive.js
+++ canvas-responsive.js
@@ -1,6 +1,7 @@
 const { Canvas, ui } = require('tabris')
 // Ref: https://developer.mozilla.org/en-US/docs/Web/API/Canvas_API/Tutorial/Drawing_shapes
-ui.contentView.background = '#e50914'
+// colors from: https://brandcolors.net/  Unity
+ui.contentView.background = '#222c37'
 
 new Canvas({ left: 0, right: 0, top: 0, bottom: 0 })
   .on('resize', ({ target: canvas, width, height }) => {
@@ -13,16 +14,22 @@ new Canvas({ left: 0, right: 0, top: 0, bottom: 0 })
     let rectWidth = 60
     let rectHeight = rectWidth
 
-    ctx.fillStyle = '#221f1faa'
+    ctx.fillStyle = '#00ccccaa'
+    // draw('#221f1faa')
     ctx.fillRect(posX - offSet, topMargin, rectWidth, rectHeight)
-    ctx.fillStyle = '#221f1f'
+    ctx.fillStyle = '#fff600aa'
     ctx.fillRect(posX, topMargin + offSet, rectWidth, rectHeight)
-    ctx.fillStyle = '#f5f5f1'
+    ctx.fillStyle = '#ff0066aa'
     ctx.fillRect(posX + offSet, topMargin + offSet * 2, rectWidth, rectHeight)
+
     ctx.font = '24px'
-    ctx.fillStyle = '#f5f5f1'
+    ctx.fillStyle = '#fff600aa'
     ctx.textAlign = 'center'
 
     ctx.fillText('Company', posX, posY + 80)
+
+    function draw (c) {
+      ctx.fillStyle = c
+    }
   })
   .appendTo(ui.contentView)
