87df347c0fdfa33a0babee3081e7a72d8c32dc2d
Rob McCormack <imedia@IMD-IM-61a-2.hitronhub.home>
2018-04-09 19:51:47 -0400

    got images to load randomly, by adjusting the size

--- composite-refresh.js
+++ composite-refresh.js
@@ -1,17 +1,17 @@
-const { RefreshComposite, CheckBox, ScrollView, TextView, ui } = require('tabris')
+const {app,  RefreshComposite, CheckBox, ImageView, ScrollView, TextView, ui} = require('tabris')
 
-let recRefresh = new RefreshComposite({
-  left: 0,
-  right: 0,
-  top: 0,
-  bottom: 0
-})
+let counter = 1
+
+let recRefresh = new RefreshComposite({ left: 0, right: 0, top: 0, bottom: 0 })
   .on(
     'refresh',
     ({ target }) => setTimeout(
       () => {
         target.refreshIndicator = false
+        // app.reload()
         textView.text = `last refresh: ${new Date()}\n${textView.text}`
+        imvPicture.image = 'https://source.unsplash.com/collection/1163637/160x9' + counter
+        counter = counter + 1
       },
       1000
     )
@@ -34,11 +34,22 @@ new TextView({
   text: 'pull to refresh'
 }).appendTo(scrollView)
 
+let imvPicture = new ImageView({
+  centerX: 0,
+  top: 'prev() 10',
+  width: 160,
+  height: 90,
+  image: 'https://source.unsplash.com/collection/1163637/160x90',
+  background: '#aaaaaa',
+  scaleMode: 'fill'
+}).appendTo(scrollView)
+
 const textView = new TextView({
   left: 0,
   right: 0,
   top: 'prev() 32',
   alignment: 'center',
+    font: '8px',
   lineSpacing: 1.4
 }).appendTo(scrollView)
 
