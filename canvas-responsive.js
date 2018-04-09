5d2027092e4b10d27079a029b92de44ca319bcf7
Rob McCormack <imedia@IMD-IM-61a-2.hitronhub.home>
2018-04-09 11:58:22 -0400

    fixing things, stopped working on Android, OK now.

--- canvas-responsive.js
+++ canvas-responsive.js
@@ -2,30 +2,25 @@ const { Canvas, ui } = require('tabris')
 // Ref: https://developer.mozilla.org/en-US/docs/Web/API/Canvas_API/Tutorial/Drawing_shapes
 ui.contentView.background = '#e50914'
 
-new Canvas({ centerX: 0, centerY: 0 })
+new Canvas({ left: 0, right: 0, top: 0, bottom: 0 })
   .on('resize', ({ target: canvas, width, height }) => {
     let ctx = canvas.getContext('2d', width, height)
     let posX = width / 2
     let posY = height / 2
 
     let topMargin = height / 3
-    let topOffSet = 15
+    let offSet = 15
     let rectWidth = 60
     let rectHeight = rectWidth
 
     ctx.fillStyle = '#221f1faa'
-    ctx.fillRect(posX - 40, topMargin, rectWidth, rectHeight)
+    ctx.fillRect(posX - offSet, topMargin, rectWidth, rectHeight)
     ctx.fillStyle = '#221f1f'
-    ctx.fillRect(posX, topMargin + topOffSet, rectWidth, rectHeight)
+    ctx.fillRect(posX, topMargin + offSet, rectWidth, rectHeight)
     ctx.fillStyle = '#f5f5f1'
-    ctx.fillRect(
-      posX + 25,
-      topMargin + topOffSet * 2,
-      rectWidth,
-      rectHeight
-    )
+    ctx.fillRect(posX + offSet, topMargin + offSet * 2, rectWidth, rectHeight)
     ctx.font = '24px'
-    ctx.fillStyle = '#221f1f'
+    ctx.fillStyle = '#f5f5f1'
     ctx.textAlign = 'center'
 
     ctx.fillText('Company', posX, posY + 80)
