01dcef8edabbe8a458ac7cc1089b0fcb4d92af88
Rob McCormack <imedia@IMD-IM-61a-2.hitronhub.home>
2018-04-11 18:31:02 -0400

    added marbles and improved code

--- motion-only.js
+++ motion-only.js
@@ -1,31 +1,85 @@
 const { Button, TextView, ui } = require('tabris')
 
-function onSuccess (acceleration) {
-  console.log(
-    'Acceleration X: ' +
-      acceleration.x +
-      '\n' +
-      'Acceleration Y: ' +
-      acceleration.y +
-      '\n' +
-      'Acceleration Z: ' +
-      acceleration.z +
-      '\n' +
-      'Timestamp: ' +
-      acceleration.timestamp +
-      '\n'
-  )
+function onSuccess(acceleration) {
+  txvAccelerationX.text = 'Acceleration X: ' + acceleration.x
+  txvAccelerationY.text = 'Acceleration Y: ' + acceleration.y
+  txvAccelerationZ.text = 'Acceleration Z: ' + acceleration.z
+  txvTimeStamp.text = 'Timestamp: ' + getFormattedDate(acceleration.timestamp)
 }
 
-function onError () {
+function onError() {
   console.log('onError!')
 }
 
-const options = { frequency: 3000 }
+// updates every 1/4 second, 250 ms.
 
-// Update every 3 seconds
+const options = { frequency: 250 }
+
 let watchID = navigator.accelerometer.watchAcceleration(
   onSuccess,
   onError,
   options
 )
+
+let txvAccelerationX = new TextView({
+  left: 10,
+  top: 'prev() 10',
+  right: 10,
+  text: 'Acceleration X: ',
+  alignment: 'left'
+}).appendTo(ui.contentView)
+
+let txvAccelerationY = new TextView({
+  left: 10,
+  top: 'prev() 10',
+  right: 10,
+  text: 'Acceleration Y: ',
+  alignment: 'left'
+}).appendTo(ui.contentView)
+
+let txvAccelerationZ = new TextView({
+  left: 10,
+  top: 'prev() 10',
+  right: 10,
+  text: 'Acceleration Z: ',
+  alignment: 'left'
+}).appendTo(ui.contentView)
+
+let txvTimeStamp = new TextView({
+  left: 10,
+  top: 'prev() 10',
+  right: 10,
+  text: 'Timestamp: ',
+  alignment: 'left'
+}).appendTo(ui.contentView)
+
+function getFormattedDate () {
+  let date = new Date()
+
+  let month = date.getMonth() + 1
+  let day = date.getDate()
+  let hour = date.getHours()
+  let min = date.getMinutes()
+  let sec = date.getSeconds()
+
+  month = (month < 10 ? '0' : '') + month
+  day = (day < 10 ? '0' : '') + day
+  hour = (hour < 10 ? '0' : '') + hour
+  min = (min < 10 ? '0' : '') + min
+  sec = (sec < 10 ? '0' : '') + sec
+
+  let str =
+    date.getFullYear() +
+    '-' +
+    month +
+    '-' +
+    day +
+    ' » ' +
+    hour +
+    ':' +
+    min +
+    ':' +
+    sec
+
+  return str
+}
