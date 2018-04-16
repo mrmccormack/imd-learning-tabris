b1e8b3fb1638b96f5454c2d02b5361f81b52de8d
Rob McCormack <imedia@IMD-IM-61a-2.hitronhub.home>
2018-04-16 08:35:48 -0400

    got simplified camera to work on playground

---
+++ camera.js
@@ -0,0 +1,33 @@
+const { Button, ImageView, ui } = require('tabris')
+
+function takePicture () {
+  let onSuccess = image => imgPhoto.image = image
+  let onFail = message => console.log('Camera failed because: ' + message)
+  navigator.camera.getPicture(onSuccess, onFail, {
+    quality: 50,
+    targetWidth: 1024,
+    targetHeight: 1024,
+    destinationType: window.Camera.DestinationType.FILE_URI
+  })
+}
+
+let btnShutter = new Button({
+  id: 'pictureButton',
+  centerX: 0,
+  top: 'prev() 10',
+  width: 100,
+  text: 'Tacke Picture'
+})
+  .on('select', () => {
+    console.log('hi')
+    takePicture()
+  })
+  .appendTo(ui.contentView)
+
+let imgPhoto = new ImageView({
+  top: 'prev() 10',
+  class: 'image',
+  width: 100,
+  height: 100,
+  centerX: 0
+}).appendTo(ui.contentView)
