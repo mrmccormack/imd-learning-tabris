6e4821e8cc5d79f312238eb595728d9231f8404f
Rob McCormack <imedia@IMD-IM-61a-2.hitronhub.home>
2018-04-16 09:32:50 -0400

    added const for IMGURL

--- camera-monkey.js
+++ camera-monkey.js
@@ -2,9 +2,17 @@ const { Button, ImageView, ui } = require('tabris')
 
 const widthPhoto = 300
 const heightPhoto = 400
+const IMGURL = 'https://raw.githubusercontent.com/mrmccormack/imd-learning-tabris/master/images/'
 
 function takePhoto () {
-imgPhoto.image = 'https://i.ytimg.com/vi/u5dT8Pm9ow4/hqdefault.jpg'
+  let onSuccess = image => imgPhoto.image = IMGURL + 'monkey.jpg'
+  let onFail = message => console.log('Camera failed because: ' + message)
+  navigator.camera.getPicture(onSuccess, onFail, {
+    quality: 50,
+    targetWidth: widthPhoto,
+    targetHeight: heightPhoto,
+    destinationType: window.Camera.DestinationType.FILE_URI
+  })
 }
 
 let btnShutter = new Button({ centerX: 0, top: 40, text: 'Take Picture' })
@@ -18,7 +26,7 @@ let imgPhoto = new ImageView({
   width: widthPhoto,
   height: heightPhoto,
   centerX: 0,
-  image: 'https://raw.githubusercontent.com/mrmccormack/imd-learning-tabris/master/images/photo.png',
+  image: IMGURL + 'photo.png',
   scaleMode: 'fit',
   zoomEnabled: true
 }).appendTo(ui.contentView)
