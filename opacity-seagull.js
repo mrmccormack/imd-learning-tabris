d85be9d943e94b81da670fb24a1b4a3e25d6ccf5
Rob McCormack <mccormac@confederationc.on.ca>
2018-05-11 12:30:30 -0400

    updated from college

--- opacity-seagull.js
+++ opacity-seagull.js
@@ -2,30 +2,28 @@ const { Button, ui, ImageView} = require('tabris');
 // example of images side by side - Matt.
 
 const IMAGE_PATH = 'https://mrmccormack.github.io/imd-learning-tabris/images/';
-const DICE_OFFSET = 30;
+const DICE_OFFSET = 0;
 
-let btnOpacity = new Button({ centerX: 0, top: 40, text: 'Take Picture' })
+let btnOpacity = new Button({ centerX: 0, top: 40, text: 'Sharpen' })
   .on('select', () => {
-    takePhoto()
-  })
-  .on('longpress', () => {
-    takeMonkey()
-  })
-  .appendTo(ui.contentView)
+    blurPhoto.visible = false
+    sharpPhoto.opacity = 1.0
+  }).appendTo(ui.contentView)
 
 
-let diceImage1 = new ImageView({
+let sharpPhoto = new ImageView({
   centerY: 0,
     width: 200,
   centerX: -DICE_OFFSET,
-  image: IMAGE_PATH + 'seagull-1900657_640-blur.jpg'
+  image: IMAGE_PATH + 'seagull-1900657_640.jpg'
 }).appendTo(ui.contentView);
 
-let diceImage2 = new ImageView({
+let blurPhoto = new ImageView({
   centerY: 0,
   width: 200,
   opacity: 0.4,
   centerX: DICE_OFFSET,
-  image: IMAGE_PATH + 'seagull-1900657_640.jpg'
+  image: IMAGE_PATH + 'seagull-1900657_640-blur.jpg'
 
 }).appendTo(ui.contentView);
+      
